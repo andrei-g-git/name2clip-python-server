d16909a3009c0a84ea125896ff51e566116f3405
unknown <andrei_g_git@protonmail.com>
2022-08-28 15:25:55 +0300

    add scraper POST route

--- server.py
+++ server.py
@@ -10,37 +10,9 @@
 
 
 
-
-# def do_GET(self):
-#     self.send_response(200)
-#     self.send_header('Access-Control-Allow-Origin', '*')           
-#     self.end_headers()
-
-
-
-
-
-# from http.server import *
-  
-# class GFG(BaseHTTPRequestHandler):
-    
-#     def do_GET(self):
-        
-#         self.send_response(200)
-          
-#         self.send_header('content-type', 'text/html')
-#         self.send_header('Access-Control-Allow-Origin', '*')    
-#         self.end_headers()
-        
-
-# port = HTTPServer(('', 5555), GFG)
-  
-# port.serve_forever()    
-
-
-
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import requests
+import json
 
 PORT = 9999
 HOST = "localhost"
@@ -59,20 +31,50 @@ class TestServer(BaseHTTPRequestHandler):
 
 
 
+    # def do_POST(self):
+
+    #     print("blaaaaaaaaaaah")
+
+    #     self.send_response(200)  
+    #     self.path = "/def"
+    #     self.send_header('Access-Control-Allow-Origin', '*')  
+    #     self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+    #     self.send_header('Access-Control-Allow-Methods', 'POST')
+    #     self.send_header("Content-type", "text/plain")
+
+    #     contentLength = int(self.headers.get("Content-length"))
+    #     body = self.rfile.read(contentLength) #hope it's a string...
+
+    #     self.end_headers()
+
+    #     print("data is: ", body.decode('utf-8')) 
+    #     response = "the request you just made is:   " +  body.decode('utf-8')
+    #     self.wfile.write(bytes(response, "utf-8"))
+
+
     def do_POST(self):
-        self.send_response(200)  
-        self.path = "/def"
-        self.send_header('Access-Control-Allow-Origin', '*')  
-        self.send_header("Content-type", "text/plain")
 
-        contentLength = int(self.headers.get("Content-length"))
-        body = self.rfile.read(contentLength) #hope it's a string...
-
+        self.send_response(200)
+        self.path = "/scraper"
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        print("data is: ", body.decode('utf-8')) 
-        response = "the request you just made is:   " +  body.decode('utf-8')
-        self.wfile.write(bytes(response, "utf-8"))
+        length = int(self.headers.get("Content-length"))
+        body = json.loads(
+            self.rfile
+                .read(length)
+                .decode("utf-8")
+        )
+
+        print("raw body:  ", body["src"])
+
+
+
+#test --works
+# req = requests.get("https://bustybloom.com")
+# print(req)
+# print(req.content)
 
 
 server = HTTPServer((HOST, PORT), TestServer)
