f44e66031ae02c0ee24f8b4c0f9f569521fbd2b8
unknown <andrei_g_git@protonmail.com>
2022-08-29 11:36:01 +0300

    add extract title, el text, sources, hrefs

--- server.py
+++ server.py
@@ -10,48 +10,18 @@
 
 
 
+from functools import reduce
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import requests
 import json
 from bs4 import BeautifulSoup
+import html
+import re
 
 PORT = 9999
 HOST = "localhost"
 
 class TestServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-
-        self.path = "/abc"
-
-        self.send_header('Access-Control-Allow-Origin', '*')        
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-
-        self.wfile.write(bytes("just some generic response", "utf-8")) 
-
-
-
-    # def do_POST(self):
-
-    #     print("blaaaaaaaaaaah")
-
-    #     self.send_response(200)  
-    #     self.path = "/def"
-    #     self.send_header('Access-Control-Allow-Origin', '*')  
-    #     self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-    #     self.send_header('Access-Control-Allow-Methods', 'POST')
-    #     self.send_header("Content-type", "text/plain")
-
-    #     contentLength = int(self.headers.get("Content-length"))
-    #     body = self.rfile.read(contentLength) #hope it's a string...
-
-    #     self.end_headers()
-
-    #     print("data is: ", body.decode('utf-8')) 
-    #     response = "the request you just made is:   " +  body.decode('utf-8')
-    #     self.wfile.write(bytes(response, "utf-8"))
-
 
     def do_POST(self): 
 
@@ -68,87 +38,40 @@ class TestServer(BaseHTTPRequestHandler):
                 .decode("utf-8")
         )
 
-        print("raw body:  ", body["src"])
+        print("src attribute:  ", body["src"])
 
         res = requests.get(body["url"])
         content = res.content
-        html = BeautifulSoup(content, "html.parser")
+        soup = BeautifulSoup(content, "html.parser")
 
         src = body["src"]
-        #string = json.dumps(src)
-        # attrDict = {"src": ""}
-        # attrDict["src"] = src
-        # selector = "[src=%s]"%src
-        #mediaTagThing = html.select("img[src='%s']"%src)
-        #mediaTagThing = html.find_all(attrs={"src": "%s"%src})
 
-        srcWithoutProtocol = src.replace("https:", "")
-        mediaTagThing = html.find_all(attrs={
-            "src": src,
-            "src": srcWithoutProtocol
-        })
+        relativeSrc = src.split("/")[-1]
 
+        print("RELATIVE SRC: ", relativeSrc)
 
-        print("src:   ", body["src"])
-        #print(string)
-        print(mediaTagThing)
-        #print(type(src), "   ", type(string))
-        #print('img[src="%s"]'%src)
-        print("is string? :", isinstance(src, str))
+        regex = "%s$"%relativeSrc
+        matchingElements = soup.find_all(attrs={"src": re.compile(r'%s'%regex)})
 
-        #find_context_element_from_all("src", src, html)
+        contextElement = matchingElements[0]
 
-def find_context_element_from_all(attributeType, attributeValue, soup):
-    tagsWithSrc = soup.select("[src]")
-    #contextElements = filter(lambda elem: (elem["src"] == attributeValue), tagsWithSrc)
+        grandparent = contextElement.parent.parent
 
-    srcWithoutProtocol = attributeValue.replace("https:", "")
+        anchors = grandparent.find_all("a")
 
+        hrefs = []
+        for a in anchors:
+            hrefs.append(a.get("href"))
 
-    contextElements = []
-    for tag in tagsWithSrc:
-        print(">>>>>>>>>>>>>    ", tag["src"])
-        if tag["src"] == attributeValue or tag["src"] == srcWithoutProtocol:
-            contextElements.append(tag)
+        title = soup.find("title").text
 
+        print("\n\n text:   ", grandparent.text)
+        print("src:  ", src)
+        print("hrefs: ", hrefs)
+        print("TITLE:  ", title)
 
-    #length = sum(1 for item in contextElements)
-    length = len(contextElements)
-    if length > 0: 
-        #print(next(contextElements))
-        print(contextElements[0])
-    else: 
-        print("contextElements is empty")
-    print(attributeValue)
-    #print(tagsWithSrc)
+        #print("\n\n ^^^^^^^^^^^^^^^^^^^^^^ \n all inner text:   ", soup.text)
 
-
-def findContextElement(src):
-    abc = src
-    #nothing works
-    #getting the element like this doesn't work 
-
-    #  element = html.find_all(attrs={"src": "%s"%src})
-
-    #feeding a dictionary doesn't work
-    #    attrDict = {"src": ""}
-    #    attrDict["src"] = src
-    #     element = html.find_all(attrs=attrDict)
-
-
-    #or using a selector ... no go
-    #      element = html.select('img[src="%s"]'%src)
-
-    #I don't goddamn know, python sucks
-
-
-
-
-
-#test --works
-# req = requests.get("https://bustybloom.com")
-# print(req)
-# print(req.content)
 
 
 server = HTTPServer((HOST, PORT), TestServer)
