fa8feb1a6ad70764ae46211241aecc8150354930
unknown <andrei_g_git@protonmail.com>
2022-09-05 17:59:04 +0300

    fix some ner mistakes

--- src/nlp_processor.py
+++ src/nlp_processor.py
@@ -4,18 +4,33 @@ class EntProcessor:
 
     def process_names_from_string(self, string):
         nlp = spacy.load("en_core_web_md")
-        doc = nlp(string)
 
-        persons = [ent.text for ent in doc.ents if ent.label_ == "PERSON"] 
+        lowerString = string.lower()
+
+        doc = nlp(lowerString)
+
+        #test
+        # for ent in doc.ents:
+        #     print("LABELS: \n", ent.text, "    ", ent.label_)
+
+
+        properNouns = "".join([(token.text + " ") for token in doc if token.pos_ == "PROPN"])
+        pnDoc = nlp(properNouns)
+
+        for token in pnDoc:
+            print("POS: \n", token.text, "   ", token.pos_)
+
+        persons = [ent.text for ent in doc.ents if ent.label_ == "PERSON"] #<--- back to doc
         orgs = [ent.text for ent in doc.ents if ent.label_ == "ORG"]
         
         print("tokens:   ", [token for token in doc])
+        print("PN tokens:   ", [token for token in pnDoc])
         print("persons:   ", persons, "   orgs:    ", orgs)
         return persons, orgs
 
     def process_names_from_lists(self, hrefs):
-        allLinks = [link for link in hrefs]
-        persons, orgs = []
+        allLinks = "".join(hrefs)#[link for link in hrefs]
+        persons, orgs = [], []
         if len(allLinks):
             persons, orgs = self.process_names_from_string(allLinks)
 
