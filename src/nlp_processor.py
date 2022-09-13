23ebce091c7ed2fc700516ba4e5edd2503e34671
unknown <andrei_g_git@protonmail.com>
2022-09-13 19:11:01 +0300

    add name-surname pattern recognition

--- src/nlp_processor.py
+++ src/nlp_processor.py
@@ -8,54 +8,59 @@ class EntProcessor:
 
     def process_names_from_string(self, string):
         #nlp = spacy.load("en_core_web_lg")
-        nlp = spacy.load("G:\\portfolio\\projects\\scraper\\models\\woman_first_names_ner_model_1")
+        nlp = spacy.load("G:\\portfolio\\projects\\scraper\\models\\woman_first_names_and_surnames_ner_model_2")
 
         #lowerString = string.lower()
 
         #test
-        string = string.replace("[M4A]", "")
+        string = string.replace("-", "").replace("|", "")
+        string = re.sub('[^a-zA-Z0-9\n\.]', ' ', string)
 
         doc = nlp(string)
 
         print("S T R I N G G G G :    ", string)
 
 
-        properNouns = " ".join([(token.text) for token in doc if token.pos_ == "PROPN"])
-        pnDoc = nlp(properNouns)
+        #properNouns = " ".join([(token.text) for token in doc if token.pos_ == "PROPN"])
+        #pnDoc = nlp(properNouns)
 
-        for token in pnDoc:
-            print("POS: \n", token.text, "   ", token.pos_)
-        for ent in pnDoc.ents:
-            print("LABELS: \n", ent.text, "    ", ent.label_)
+        # for token in pnDoc:
+        #     print("POS: \n", token.text, "   ", token.pos_)
+        # for ent in pnDoc.ents:
+        #     print("LABELS: \n", ent.text, "    ", ent.label_)
 
         # persons = [ent.text for ent in pnDoc.ents if ent.label_ == "PERSON"]
         # orgs = [ent.text for ent in pnDoc.ents if ent.label_ == "ORG"]
-        persons = [ent.text for ent in doc.ents if ent.label_ == "PERSON"]
-        orgs = [ent.text for ent in doc.ents if ent.label_ == "ORG"]
+        persons = [ent.text for ent in doc.ents if ent.label_ == "NAME_SURNAME"]
+        names = [ent.text for ent in doc.ents if ent.label_ == "NAME"]
+        #orgs = [ent.text for ent in doc.ents if ent.label_ == "ORG"]
         
         if '-' in persons: 
             persons.remove('-')
-        if '-' in orgs: 
-            orgs.remove('-')
+        if '-' in names: 
+            names.remove('-')            
+        # if '-' in orgs: 
+        #     orgs.remove('-')
 
         #test
-        pattern = [{"ENT_TYPE": "PERSON"}, {"ENT_TYPE": "PERSON"}] #good, if it's separated by comma it won't match
-        matcher = Matcher(nlp.vocab)
-        matcher.add("first-last-names", [pattern])
-        matches = matcher(doc)
-        for match_id, start, end in matches:
-            #string_id = nlp.vocab.strings[match_id]  # Get string representation
-            span = doc[start:end]  # The matched span
-            print("*************************** \n first and last name: ", span.text)
+        # pattern = [{"ENT_TYPE": "PERSON"}, {"ENT_TYPE": "PERSON"}] #good, if it's separated by comma it won't match
+        # matcher = Matcher(nlp.vocab)
+        # matcher.add("first-last-names", [pattern])
+        # matches = matcher(doc)
+        # for match_id, start, end in matches:
+        #     #string_id = nlp.vocab.strings[match_id]  # Get string representation
+        #     span = doc[start:end]  # The matched span
+        #     print("*************************** \n first and last name: ", span.text)
 
 
 
         print("tokens:   ", [token for token in doc])
-        print("PN tokens:   ", [token for token in pnDoc])
-        print("persons:   ", persons, "   orgs:    ", orgs)
+        #print("PN tokens:   ", [token for token in pnDoc])
+        print("persons:   ", persons)#, "   orgs:    ", orgs)
 
         if len(persons): return persons[0] 
-        elif len(orgs): return orgs[0]
+        #elif len(orgs): return orgs[0]
+        elif len(names): return names[0]
         else: return ""
         
 
